e7afcf69f3a7500d31206e78a9af5367eacd4081
Denis Kolenteev <kolenteevdenis@gmail.com>
2024-09-09 15:07:55 +0300

    add new func Max, Yn

--- mymath.go
+++ mymath.go
@@ -10,3 +10,11 @@ func Sqrt(x float64) int {
 func Abs(x float64) float64 {
 	return math.Abs(x)
 }
+
+func Max(x, y float64) float64 {
+	return math.Max(x, y)
+}
+
+func Yn(n int, x float64) float64 {
+	return math.Yn(n, x)
+}
